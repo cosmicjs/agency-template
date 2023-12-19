8674c9cadf60b7c6ed18984aa544dd86280d8d5f
Karl Koch <karl@kejk.tech>
2023-12-19 14:53:20 +0000

    fix: services

--- app/services/[slug]/page.tsx
+++ app/services/[slug]/page.tsx
@@ -18,8 +18,8 @@ export default async function SingleProductPage({ params }: { params: { slug: st
           <ol role='list' className='flex space-x-2'>
             <li>
               <div className='flex items-center'>
-                <Link href='/shop' className='mr-2 text-sm font-medium text-gray-900 dark:text-white'>
-                  Shop
+                <Link href='/services' className='mr-2 text-sm font-medium text-gray-900 dark:text-white'>
+                  Services
                 </Link>
                 <svg width='16' height='20' viewBox='0 0 16 20' fill='currentColor' aria-hidden='true' className='h-5 w-4 text-gray-300'>
                   <path d='M5.697 4.34L8.98 16.532h1.327L7.025 4.341H5.697z' />
