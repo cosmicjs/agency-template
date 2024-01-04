bf86615f1940796f1b7f6189f76fad741ded81be
kemiljk <karl@kejk.tech>
2024-01-04 16:43:06 +0000

    fix: new image, improve: source link

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,17 +1,15 @@
+import { ChevronRight } from 'lucide-react';
+
 export default function Banner(): JSX.Element {
   return (
-    <div className="flex justify-center space-x-1 bg-zinc-100 p-2 text-xs dark:bg-zinc-900 md:text-sm lg:text-base">
-      <span className="text-zinc-800 dark:text-zinc-200">
-        The source code for this template is
-      </span>
-      <a
-        href="https://github.com/cosmicjs/blocks-demo"
-        target="_parent"
-        className="text-green-500 underline dark:text-green-300"
-      >
-        available on GitHub
+    <div className='flex justify-center mt-2'>
+      <a href='https://github.com/cosmicjs/blocks-demo' target='_parent'>
+        <div className='flex items-center space-x-1 bg-zinc-100 px-4 py-2 text-xs dark:bg-zinc-800 md:text-sm lg:text-base rounded-full'>
+          <span className='text-zinc-800 dark:text-zinc-200'>Source code available on</span>
+          <span className='text-green-500 font-medium dark:text-green-300'>GitHub</span>
+          <ChevronRight className='w-4 h-4' />
+        </div>
       </a>
-      .
     </div>
   );
 }
