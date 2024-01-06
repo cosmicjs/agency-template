354b65a3ba818c6a1cd54b48ff0b50db636f6686
Tony Spiro <tspiro@tonyspiro.com>
2024-01-05 17:09:09 -0800

    edit: github link

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -3,7 +3,7 @@ import { ChevronRight } from "lucide-react"
 export default function Banner(): JSX.Element {
   return (
     <div className="flex justify-center mt-2">
-      <a href="https://github.com/cosmicjs/blocks-demo" target="_parent">
+      <a href="https://github.com/cosmicjs/agency-template" target="_parent">
         <div className="flex items-center space-x-1 bg-zinc-100 px-4 py-2 text-xs dark:bg-zinc-800 md:text-sm lg:text-base rounded-full">
           <span className="text-zinc-800 dark:text-zinc-200">
             Source code available on
