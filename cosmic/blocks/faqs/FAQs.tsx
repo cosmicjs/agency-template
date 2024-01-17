a8d743c754ad9bfb74442ffb638ce4f384489adc
Tony Spiro <tspiro@tonyspiro.com>
2024-01-17 08:52:08 -0800

    edit: FAQs Block update

--- cosmic/blocks/faqs/FAQs.tsx
+++ cosmic/blocks/faqs/FAQs.tsx
@@ -15,15 +15,18 @@ type FAQ = {
 export async function FAQs({
   query,
   className,
+  status,
 }: {
   query: any
   className?: string
+  status?: "draft" | "published" | "any"
 }) {
   const { object: page } = await cosmic.objects
     .findOne(query)
     .props("slug,title,metadata")
     .depth(1)
-
+    .status(status ? status : "published")
+  if (!page?.metadata?.faqs) return <></>
   return (
     <div className={className}>
       {(page?.metadata?.faqs).map((faq: FAQ) => {
