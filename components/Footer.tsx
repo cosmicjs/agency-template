c3977d6fd38fce7b40fb7528c17a2c68f3b1461c
Tony Spiro <tspiro@tonyspiro.com>
2024-01-17 08:29:41 -0800

    edit: NavMenu Block update

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import { cosmic } from "@/cosmic/client"
 import { buttonVariants } from "@/cosmic/elements/Button"
 import { MailIcon, PhoneIcon } from "lucide-react"
 import { ModeToggle } from "./theme-toggle"
-import { FooterNavMenu } from "@/cosmic/blocks/navigation-menu/NavMenu"
+import { NavMenu } from "@/cosmic/blocks/navigation-menu/NavMenu"
 
 export default async function Footer() {
   const { object: settings } = await cosmic.objects
@@ -25,7 +25,7 @@ export default async function Footer() {
   return (
     <nav className="pb-8 md:my-10 mx-auto flex flex-col lg:flex-row items-center justify-between container w-full">
       <div className="my-8">
-        <FooterNavMenu query={{ type: "navigation-menus", slug: "footer" }} />
+        <NavMenu query={{ type: "navigation-menus", slug: "footer" }} />
       </div>
       <div className="flex mb-6 lg:mb-0 gap-x-8 justify-center text-zinc-700 dark:text-zinc-300">
         <div>
