43c70d1f5439559ec1864fa851bead5ae7663d36
Tony Spiro <tspiro@tonyspiro.com>
2024-01-17 10:37:56 -0800

    fix: home mobile padding

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default async function HomePage() {
   return (
     <main>
       <Page query={{ slug: "home", type: "pages" }} />
-      <section className="md:container mt-12 pb-8 m-auto">
+      <section className="md:container mt-12 pb-8 m-auto px-4">
         <div className="relative m-auto flex max-w-[950px] flex-col items-start gap-2">
           <h3 className="m-auto mb-4 text-2xl md:text-4xl font-display text-zinc-900 dark:text-zinc-100 tracking-tighter">
             Hear from our customers
@@ -16,7 +16,7 @@ export default async function HomePage() {
         </div>
       </section>
 
-      <section className="md:container mt-12 pb-8 m-auto">
+      <section className="md:container mt-12 pb-8 m-auto px-4">
         <div className="max-w-4xl mx-auto">
           <h2 className="text-2xl font-semibold mb-4 text-zinc-800 dark:text-zinc-100">
             Frequently Asked Questions
