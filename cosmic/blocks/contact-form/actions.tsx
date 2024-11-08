0766ffe9d38e1fef00b03b826b78457944e6c1a1
Tony Spiro <tspiro@tonyspiro.com>
2024-11-08 10:20:01 -0800

    fix: resend variable issue

--- cosmic/blocks/contact-form/actions.tsx
+++ cosmic/blocks/contact-form/actions.tsx
@@ -1,26 +1,26 @@
-"use server"
+"use server";
 
-import { cosmic } from "@/cosmic/client"
-import { Resend } from "resend"
-const RESEND_KEY = process.env.RESEND_API_KEY
+import { cosmic } from "@/cosmic/client";
+import { Resend } from "resend";
+const RESEND_KEY = process.env.RESEND_API_KEY;
 const CONTACT_EMAIL =
-  process.env.CONTACT_EMAIL || "change_to_your_email@example.com"
-const resend = new Resend(RESEND_KEY)
+  process.env.CONTACT_EMAIL || "change_to_your_email@example.com";
+const resend = new Resend(RESEND_KEY);
 
 export type AddSubmissionType = {
-  type: "form-submissions"
-  title: string
+  type: "form-submissions";
+  title: string;
   metadata: {
-    email: string
-    company: string
-    message: string
-  }
-}
+    email: string;
+    company: string;
+    message: string;
+  };
+};
 
 export async function addSubmission(comment: AddSubmissionType) {
-  const { metadata: metadata, title } = comment
-  const data = await cosmic.objects.insertOne(comment)
-  const submitterSubject = `Form submission received`
+  const { metadata: metadata, title } = comment;
+  const data = await cosmic.objects.insertOne(comment);
+  const submitterSubject = `Form submission received`;
   const submitterHTML = `
     Hello ${title},<br/><br/>
     This is a message to confirm that we have received your form submission with the following information:<br/><br/>
@@ -30,7 +30,7 @@ export async function addSubmission(comment: AddSubmissionType) {
     Message: ${metadata.message}<br/>
     <br/>
     A representative will be in touch with you soon.
-  `
+  `;
   // Send confirmation email
   await sendEmail({
     to: metadata.email,
@@ -38,15 +38,15 @@ export async function addSubmission(comment: AddSubmissionType) {
     reply_to: CONTACT_EMAIL,
     subject: submitterSubject,
     html: submitterHTML,
-  })
-  const adminSubject = `${title} submitted the form`
+  });
+  const adminSubject = `${title} submitted the form`;
   const adminHTML = `
     ${title} submitted the contact form with the following information:<br/><br/>
     Name: ${title}<br/>
     Email: ${metadata.email}<br/>
     Company: ${metadata.company}<br/>
     Message: ${metadata.message}<br/>
-  `
+  `;
   // Send email to admin
   await sendEmail({
     to: CONTACT_EMAIL,
@@ -54,8 +54,8 @@ export async function addSubmission(comment: AddSubmissionType) {
     reply_to: metadata.email,
     subject: adminSubject,
     html: adminHTML,
-  })
-  return data
+  });
+  return data;
 }
 
 async function sendEmail({
@@ -65,18 +65,18 @@ async function sendEmail({
   html,
   reply_to,
 }: {
-  from: string
-  to: string
-  subject: string
-  html: string
-  reply_to: string
+  from: string;
+  to: string;
+  subject: string;
+  html: string;
+  reply_to: string;
 }) {
   const data = await resend.emails.send({
     from,
     to,
     subject,
     html,
-    reply_to,
-  })
-  return data
+    replyTo: reply_to,
+  });
+  return data;
 }
